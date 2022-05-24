fe837d50bb5a708dde61c559d9b1e7cdb464360a
Lethaline <LemmensLouis@outlook.fr>
2022-05-24 20:09:54 +0200

    Lethaline

--- Lust.py
+++ Lust.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from dotenv import load_dotenv
+from discord import Intents
 from discord_slash import SlashCommand, SlashContext
 from Functions.Randomize import *
 import os
@@ -8,13 +9,17 @@ def main():
     load_dotenv()
     TOKEN = os.getenv('DISCORD_TOKEN')
     PREFIX = os.getenv('DISCORD_PREFIX')
-    bot = commands.Bot(command_prefix=PREFIX)
+    bot = commands.Bot(command_prefix=PREFIX, intents=Intents.all())
+    slash = SlashCommand(bot, sync_commands=True)
     for files in os.listdir('./Commands'):
         if files.endswith('.py'):
            bot.load_extension(f'Commands.{files[:-3]}')
     for listeners in os.listdir('./Listeners'):
         if listeners.endswith('.py'):
            bot.load_extension(f'Listeners.{listeners[:-3]}')
+    for slashcommands in os.listdir('./Slash_Commands'):
+        if slashcommands.endswith('.py'):
+           bot.load_extension(f'Slash_Commands.{slashcommands[:-3]}')
     bot.run(TOKEN)
     
 if __name__ == '__main__':
